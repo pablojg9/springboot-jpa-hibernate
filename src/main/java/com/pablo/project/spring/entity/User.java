788e18c96db1dbfe2bf8e26e0e19c0a63c3fcd97
pablojg9 <pablojuniorgn2@gmail.com>
2021-10-01 12:28:49 -0300

    order, instant

--- src/main/java/com/pablo/project/spring/entity/User.java
+++ src/main/java/com/pablo/project/spring/entity/User.java
@@ -1,7 +1,10 @@
 package com.pablo.project.spring.entity;
 
+
 import javax.persistence.*;
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Objects;
 
 @Entity
@@ -17,6 +20,9 @@ public class User implements Serializable {
     private String email;
     private String phone;
     private String password;
+
+    @OneToMany(mappedBy = "client")
+    private List<Order> orders = new ArrayList<>();
 
     public User() {
     }
@@ -67,6 +73,10 @@ public class User implements Serializable {
 
     public void setPassword(String password) {
         this.password = password;
+    }
+
+    public List<Order> getOrders() {
+        return orders;
     }
 
     @Override
