c56d3b8f7587bdd3a03a4e78a2a15aeb1de20f69
pablojg9 <pablojuniorgn2@gmail.com>
2021-10-14 20:06:37 -0300

    OrderItem

---
+++ src/main/java/com/pablo/project/spring/entity/pk/OrderItemPK.java
@@ -0,0 +1,53 @@
+package com.pablo.project.spring.entity.pk;
+
+import com.pablo.project.spring.entity.Order;
+import com.pablo.project.spring.entity.Product;
+
+import javax.persistence.Embeddable;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import java.io.Serializable;
+import java.util.Objects;
+
+@Embeddable
+public class OrderItemPK implements Serializable {
+
+    private static final Long serialVersionUID = 1L;
+
+    @ManyToOne
+    @JoinColumn(name = "fk_order_id")
+    private Order order;
+
+    @ManyToOne
+    @JoinColumn(name = "fk_product_id")
+    private Product product;
+
+    public Order getOrder() {
+        return order;
+    }
+
+    public void setOrder(Order order) {
+        this.order = order;
+    }
+
+    public Product getProduct() {
+        return product;
+    }
+
+    public void setProduct(Product product) {
+        this.product = product;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        OrderItemPK that = (OrderItemPK) o;
+        return Objects.equals(order, that.order) && Objects.equals(product, that.product);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(order, product);
+    }
+}
